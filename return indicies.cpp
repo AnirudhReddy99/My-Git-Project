f0aefcb97cc902fa84f968886fe6acc44ce594fe
AnirudhReddy99 <122590791+AnirudhReddy99@users.noreply.github.com>
2023-01-14 23:45:09 -0600

    return indices using c++
    This following program represents the task to accomplish, Given an array of integer nums and an integer target, return indices of the two numbers such that they add up to the target.

---
+++ return indicies.cpp
@@ -0,0 +1,15 @@
+class solution {
+    public int[] twosum(int[] nums, int target) {
+          
+          for(int i=0; i<nums.lenght; i++)
+         {
+              for (int j=i+1; j<nums.lenght; j++)
+              {
+                    if(nums[i]+nums[j]==target)
+                        return new int[]{i, j};
+              }
+         }
+         throw new IllegalArgumentException("Invalid elements"); 
+     
+     }
+}
